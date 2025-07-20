6f4c6d3c4b55bc8dd100f925da7d2441072d1183
markfisher <mark@modulewise.com>
2025-07-20 17:12:37 -0400

    add incrementor
    Signed-off-by: markfisher <mark@modulewise.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cargo component build -p calculator --target wasm32-unknown-unknown --release
 cargo component build -p counter --target wasm32-unknown-unknown --release
 cargo component build -p greeter --target wasm32-unknown-unknown --release
+cargo component build -p incrementor --target wasm32-unknown-unknown --release
 
 cp target/wasm32-unknown-unknown/release/*.wasm lib/
 
@@ -17,5 +18,7 @@ static-config -p greeting="Aloha" -o ./lib/aloha-config.wasm
 wac plug ./lib/greeter.wasm --plug ./lib/aloha-config.wasm -o ./lib/aloha.wasm
 
 wkg oci pull -o ./lib/valkey-client.wasm ghcr.io/componentized/valkey/valkey-client:v0.1.1
-wac plug ./lib/counter.wasm --plug ./lib/valkey-client.wasm -o ./lib/valkey-counter.wasm
-wac plug ./lib/valkey-counter.wasm --plug ./lib/empty-config.wasm -o ./lib/default-counter.wasm
+wac plug ./lib/incrementor.wasm --plug ./lib/valkey-client.wasm -o ./lib/valkey-incrementor.wasm
+wac plug ./lib/valkey-incrementor.wasm --plug ./lib/empty-config.wasm -o ./lib/default-incrementor.wasm
+
+wac plug ./lib/counter.wasm --plug ./lib/valkey-incrementor.wasm -o ./lib/valkey-counter.wasm
